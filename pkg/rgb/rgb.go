0fad5930c81adf840ec081a1087b7dc1216fa7f8
Tyler Kropp <kropptyler@gmail.com>
2018-07-20 23:42:39 -0500

    Add Saturate function to package rgb and test cases

--- pkg/rgb/rgb.go
+++ pkg/rgb/rgb.go
@@ -2,6 +2,8 @@ package rgb
 
 import (
 	"image"
+
+	colorful "github.com/lucasb-eyer/go-colorful"
 )
 
 // GrayImage returns a pixel-gray copy of the given image
@@ -15,4 +17,42 @@ func GrayImage(img image.Image) image.Image {
 		}
 	}
 	return gImg
+}
+
+// Saturate returns a saturated copy of the given image by the given percent
+// modify should be in range -1.0 to 1.0
+func Saturate(img image.Image, modify float64) image.Image {
+	if modify > 1.0 {
+		modify = 1.0
+	}
+	if modify < -1.0 {
+		modify = -1.0
+	}
+	sImg := image.NewRGBA(img.Bounds())
+	size := img.Bounds().Size()
+	for i := 0; i < size.X; i++ {
+		for j := 0; j < size.Y; j++ {
+			col, ok := colorful.MakeColor(img.At(i, j))
+			if !ok {
+				sImg.Set(i, j, col)
+				continue
+			}
+			h, s, v := col.Hsv()
+			var newSat, factor float64
+			if modify >= 0 {
+				room := 1 - s
+				grey := s
+				if s > 0.5 {
+					grey = 1
+				}
+				factor = modify * room * grey
+			} else {
+				room := s
+				factor = modify * room
+			}
+			newSat = s + factor
+			sImg.Set(i, j, colorful.Hsv(h, newSat, v))
+		}
+	}
+	return sImg
 }
--- pkg/rgb/rgb_test.go
+++ pkg/rgb/rgb_test.go
@@ -36,3 +36,40 @@ func TestGrayImage(t *testing.T) {
 		})
 	}
 }
+
+func TestSaturate(t *testing.T) {
+	iImg := image.NewRGBA(image.Rect(0, 0, 1, 1))
+	col := color.RGBA{0x00, 0x00, 0xff, 0xff}
+	iImg.SetRGBA(0, 0, col)
+	wImg := image.NewRGBA(image.Rect(0, 0, 1, 1))
+	col = color.RGBA{0xff, 0xff, 0xff, 0xff}
+	wImg.SetRGBA(0, 0, col)
+	zImg := image.NewRGBA(image.Rect(0, 0, 1, 1))
+	col = color.RGBA{0xff, 0xff, 0xff, 0x00}
+	zImg.SetRGBA(0, 0, col)
+	bImg := image.NewRGBA(image.Rect(0, 0, 1, 1))
+	col = color.RGBA{0x00, 0x00, 0x00, 0xff}
+	bImg.SetRGBA(0, 0, col)
+	type args struct {
+		img    image.Image
+		modify float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want image.Image
+	}{
+		{"OverflowSaturate", args{iImg, 5.0}, iImg},
+		{"IdentitySaturate", args{iImg, 1.0}, iImg},
+		{"IdentityDesaturate", args{iImg, -1.0}, wImg},
+		{"UnderflowDesaturate", args{iImg, -5.0}, wImg},
+		{"ZeroAlpha", args{zImg, 0.5}, bImg},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Saturate(tt.args.img, tt.args.modify); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Saturate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
