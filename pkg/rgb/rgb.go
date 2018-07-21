97d2fb74fe766a20bbd92db999ca3128a6f3299c
Tyler Kropp <kropptyler@gmail.com>
2018-07-20 23:41:18 -0500

    Update GrayImage documentation and remove useless struct

--- pkg/rgb/rgb.go
+++ pkg/rgb/rgb.go
@@ -4,14 +4,13 @@ import (
 	"image"
 )
 
-// GrayImage turns any image into its pixel-gray counterpart
+// GrayImage returns a pixel-gray copy of the given image
 func GrayImage(img image.Image) image.Image {
-	gray := &image.Gray{}
 	gImg := image.NewGray(img.Bounds())
 	size := img.Bounds().Size()
 	for x := 0; x < size.X; x++ {
 		for y := 0; y < size.Y; y++ {
-			g := gray.ColorModel().Convert(img.At(x, y))
+			g := gImg.ColorModel().Convert(img.At(x, y))
 			gImg.Set(x, y, g)
 		}
 	}
